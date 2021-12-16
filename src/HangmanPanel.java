b4fcd7fe9fca9fe840158e503686cdaa2b0d81d4
Siddhant Agarwal <agarwalsiddhant53@gmail.com>
2021-12-16 21:23:18 +0530

    Got scalable perfect circle working

--- src/HangmanPanel.java
+++ src/HangmanPanel.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.*;
 import javax.swing.*;
 
 public class HangmanPanel extends JPanel {
@@ -21,5 +22,23 @@ public class HangmanPanel extends JPanel {
 
         label = new JLabel("Hangman Panel");
         add(label);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setStroke(new BasicStroke(1));
+
+        int x = getWidth();
+        int y = getHeight();
+
+        int boxSize = Math.min(x, y);
+        double border = boxSize * 0.5;
+        double radius = boxSize - border;
+
+        Ellipse2D.Double circle = new Ellipse2D.Double((x / 2) - (radius / 2), (y / 2) - (radius / 2), radius, radius);
+
+        g2.draw(circle);
     }
 }
