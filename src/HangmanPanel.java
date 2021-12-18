d3c126be1338b0f24f41c262495ccabba0834bf2
Siddhant Agarwal <agarwalsiddhant53@gmail.com>
2021-12-19 00:25:03 +0530

    Draw hangman

--- src/HangmanPanel.java
+++ src/HangmanPanel.java
@@ -1,6 +1,5 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.geom.*;
 import javax.swing.*;
 
 public class HangmanPanel extends JPanel {
@@ -24,7 +23,7 @@ public class HangmanPanel extends JPanel {
         label = new JLabel("Hangman Panel");
         add(label);
 
-        gRemaining = 6;
+        gRemaining = 0;
     }
 
     public void paintComponent(Graphics g) {
@@ -36,6 +35,13 @@ public class HangmanPanel extends JPanel {
         int x = getWidth();
         int y = getHeight();
 
+        int boxSize = Math.min(x, y);
+        int border = (int) (boxSize * 0.97);
+        int radius = boxSize - border;
+
+        int cCenterY = (int) (y * 0.3 + y * 0.05) + radius;
+        int cCenterX = (int) (x / 2);
+
         g2.drawLine((int) (x * 0.2), (int) (y * 0.8), (int) (x * 0.8), (int) (y * 0.8));
         g2.drawRect((int) (x / 2 - x * 0.2), (int) (y * 0.8 - y * 0.075),(int) (x * 0.4),(int) (y * 0.075));
         g2.drawLine((int)(x / 2 + x * 0.2 - x * 0.035), (int) (y * 0.8 - y * 0.075), 
@@ -43,12 +49,26 @@ public class HangmanPanel extends JPanel {
         g2.drawLine((int) (x / 2 + x * 0.2 - x * 0.035), (int) (y * 0.3), (int) (x / 2), (int) (y * 0.3));
         g2.drawLine((int) (x / 2), (int) (y * 0.3), (int) (x / 2),(int) (y * 0.3 + y * 0.05));
 
-        // int boxSize = Math.min(x, y);
-        // double border = boxSize * 0.5;
-        // double radius = boxSize - border;
+        if(gRemaining <= 5)
+            g2.drawOval(cCenterX - radius, cCenterY - radius, radius * 2, radius * 2);
 
+        if(gRemaining <= 4) 
+            g2.drawLine(cCenterX, cCenterY + radius, cCenterX,(int) (cCenterY + y * 0.19));
 
-        // Ellipse2D.Double circle = new Ellipse2D.Double((x / 2) - (radius / 2), (y / 2) - (radius / 2), radius, radius);
-        // g2.draw(circle);
+        if(gRemaining <= 3)
+            g2.drawLine(cCenterX, cCenterY + radius + (int) (y * 0.19 * 0.07),(int) (cCenterX - x * 0.05), 
+                    cCenterY + radius + (int) (y * 0.19 * 0.23));
+
+        if(gRemaining <= 2)
+            g2.drawLine(cCenterX, cCenterY + radius + (int) (y * 0.19 * 0.07),(int) (cCenterX + x * 0.05), 
+                    cCenterY + radius + (int) (y * 0.19 * 0.23));
+        
+        if(gRemaining <= 1)
+            g2.drawLine(cCenterX, (int) (cCenterY + y * 0.19 - y * 0.19 * 0.005), (int) (cCenterX - x * 0.05), 
+                    (int) (cCenterY + radius + y * 0.19 + y * 0.19 * 0.23));
+
+        if(gRemaining == 0)
+            g2.drawLine(cCenterX, (int) (cCenterY + y * 0.19 - y * 0.19 * 0.005), (int) (cCenterX + x * 0.05), 
+                    (int) (cCenterY + radius + y * 0.19 + y * 0.19 * 0.23));
     }
 }
