43b5b2f5bb64fd860456430840c1ec763b3e60f4
Siddhant Agarwal <agarwalsiddhant53@gmail.com>
2021-12-19 10:58:50 +0530

    made graphics

--- src/HangmanPanel.java
+++ src/HangmanPanel.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 import javax.swing.*;
 
 public class HangmanPanel extends JPanel {
-    private JLabel label;
     private JPanel cards;
     private JButton back;
     private int gRemaining;
@@ -26,14 +25,11 @@ public class HangmanPanel extends JPanel {
 
         add(back);
 
-        label = new JLabel("Hangman Panel");
-        add(label);
-
-        gRemaining = 6;
+        gRemaining = 0;
 
         word = "";
         try {
-            int index = (int) (Math.random() * 2749 + 1);
+            int index = ((int) (Math.random() * 2750)) + 1;
             Scanner in = new Scanner(new File("words.txt"));
             for(int i = 1; i <= index; i++) {
                 word = in.nextLine();
@@ -44,7 +40,6 @@ public class HangmanPanel extends JPanel {
         }
 
         word.toLowerCase();
-        System.out.println(word);
 
         guess = "";
         for(int i = 0; i < word.length(); i++)
@@ -59,7 +54,42 @@ public class HangmanPanel extends JPanel {
 
         int x = getWidth();
         int y = getHeight();
+        drawGuess(g2, x, y);
+        drawGallow(g2, x, y);
+        drawMan(g2, x, y);   
+    }
 
+    private void drawGuess(Graphics2D g2, int x, int y) {
+        Font font = g2.getFont();
+        g2.setFont(new Font(font.getName(), Font.PLAIN, (int) (Math.min(x,y) * 0.05)));
+        FontMetrics fm = g2.getFontMetrics();
+        int l = guess.length();
+
+        int yC = (int) (y * 0.05);
+        int height = (int) (y * 0.15);
+        int width = (int) ((x * 0.96) / l);
+        int xC = (int) (x * 0.02);
+
+        for(int i = 0; i < l; i++) {
+            g2.drawRect(xC, yC, (int)(width - (x * 0.005)), height);
+            String letter = guess.substring(i, i + 1);
+            if(!letter.equals("_"))
+                g2.drawString(letter, (int) (xC + (width - (x * 0.005) - fm.stringWidth(letter)) / 2),(int) (yC + (((height - fm.getHeight()) / 2) + fm.getAscent())));
+            
+            xC += width;
+        }
+    }
+
+    private void drawGallow(Graphics2D g2, int x, int y) {
+        g2.drawLine((int) (x * 0.2), (int) (y * 0.8), (int) (x * 0.8), (int) (y * 0.8));
+        g2.drawRect((int) (x / 2 - x * 0.2), (int) (y * 0.8 - y * 0.075), (int) (x * 0.4), (int) (y * 0.075));
+        g2.drawLine((int) (x / 2 + x * 0.2 - x * 0.035), (int) (y * 0.8 - y * 0.075),
+                (int) (x / 2 + x * 0.2 - x * 0.035), (int) (y * 0.3));
+        g2.drawLine((int) (x / 2 + x * 0.2 - x * 0.035), (int) (y * 0.3), (int) (x / 2), (int) (y * 0.3));
+        g2.drawLine((int) (x / 2), (int) (y * 0.3), (int) (x / 2), (int) (y * 0.3 + y * 0.05));
+    }
+
+    private void drawMan(Graphics2D g2, int x, int y) {
         int boxSize = Math.min(x, y);
         int border = (int) (boxSize * 0.97);
         int radius = boxSize - border;
@@ -67,33 +97,26 @@ public class HangmanPanel extends JPanel {
         int cCenterY = (int) (y * 0.3 + y * 0.05) + radius;
         int cCenterX = (int) (x / 2);
 
-        g2.drawLine((int) (x * 0.2), (int) (y * 0.8), (int) (x * 0.8), (int) (y * 0.8));
-        g2.drawRect((int) (x / 2 - x * 0.2), (int) (y * 0.8 - y * 0.075),(int) (x * 0.4),(int) (y * 0.075));
-        g2.drawLine((int)(x / 2 + x * 0.2 - x * 0.035), (int) (y * 0.8 - y * 0.075), 
-                (int) (x / 2 + x * 0.2 - x * 0.035), (int) (y * 0.3));
-        g2.drawLine((int) (x / 2 + x * 0.2 - x * 0.035), (int) (y * 0.3), (int) (x / 2), (int) (y * 0.3));
-        g2.drawLine((int) (x / 2), (int) (y * 0.3), (int) (x / 2),(int) (y * 0.3 + y * 0.05));
-
-        if(gRemaining <= 5)
+        if (gRemaining <= 5)
             g2.drawOval(cCenterX - radius, cCenterY - radius, radius * 2, radius * 2);
 
-        if(gRemaining <= 4) 
-            g2.drawLine(cCenterX, cCenterY + radius, cCenterX,(int) (cCenterY + y * 0.19));
+        if (gRemaining <= 4)
+            g2.drawLine(cCenterX, cCenterY + radius, cCenterX, (int) (cCenterY + y * 0.19));
 
-        if(gRemaining <= 3)
-            g2.drawLine(cCenterX, cCenterY + radius + (int) (y * 0.19 * 0.07),(int) (cCenterX - x * 0.05), 
+        if (gRemaining <= 3)
+            g2.drawLine(cCenterX, cCenterY + radius + (int) (y * 0.19 * 0.07), (int) (cCenterX - x * 0.05),
                     cCenterY + radius + (int) (y * 0.19 * 0.23));
 
-        if(gRemaining <= 2)
-            g2.drawLine(cCenterX, cCenterY + radius + (int) (y * 0.19 * 0.07),(int) (cCenterX + x * 0.05), 
+        if (gRemaining <= 2)
+            g2.drawLine(cCenterX, cCenterY + radius + (int) (y * 0.19 * 0.07), (int) (cCenterX + x * 0.05),
                     cCenterY + radius + (int) (y * 0.19 * 0.23));
-        
-        if(gRemaining <= 1)
-            g2.drawLine(cCenterX, (int) (cCenterY + y * 0.19 - y * 0.19 * 0.005), (int) (cCenterX - x * 0.05), 
+
+        if (gRemaining <= 1)
+            g2.drawLine(cCenterX, (int) (cCenterY + y * 0.19 - y * 0.19 * 0.005), (int) (cCenterX - x * 0.05),
                     (int) (cCenterY + radius + y * 0.19 + y * 0.19 * 0.23));
 
-        if(gRemaining == 0)
-            g2.drawLine(cCenterX, (int) (cCenterY + y * 0.19 - y * 0.19 * 0.005), (int) (cCenterX + x * 0.05), 
+        if (gRemaining == 0)
+            g2.drawLine(cCenterX, (int) (cCenterY + y * 0.19 - y * 0.19 * 0.005), (int) (cCenterX + x * 0.05),
                     (int) (cCenterY + radius + y * 0.19 + y * 0.19 * 0.23));
     }
 }
