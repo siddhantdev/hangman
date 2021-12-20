de4288bc0310185c4e5b24709e1d8b7535775ef7
Siddhant Agarwal <agarwalsiddhant53@gmail.com>
2021-12-20 14:54:56 +0530

    added very basic info screen

--- src/InfoPanel.java
+++ src/InfoPanel.java
@@ -3,7 +3,6 @@ import java.awt.*;
 import javax.swing.*;
 
 public class InfoPanel extends JPanel {
-    private JLabel label;
     private JPanel cards;
     private JButton back;
     private JButton play;
@@ -18,10 +17,32 @@ public class InfoPanel extends JPanel {
             }
         });
 
-        add(back);
+        setLayout(new GridLayout(3, 1));
 
-        label = new JLabel("Info Panel");
-        add(label);
+        JPanel bP = new JPanel();
+        bP.add(back);
+
+        add(bP);
+
+        JTextArea area = new JTextArea();
+        area.setEditable(false);
+        area.setLineWrap(true);
+        area.setBorder(BorderFactory.createCompoundBorder(
+            area.getBorder(),
+            BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+        area.setColumns(20);
+        area.setText("The player has to guess a word");
+        area.append(" is picked by the computer. The word to");
+        area.append("guess is represented by a row of rectangles,");
+        area.append("each representing a letter of the word. If the");
+        area.append("player guesses a letter which occurs in the word,");
+        area.append("the corresponding rectangle(s) will be filled in ");
+        area.append("with the letter. However, if the letter does not ");
+        area.append("occur in the word, then a part of the hanged stick ");
+        area.append("figure will be drawn. \n\n If the hanged man appears");
+        area.append("fully before the player correctly guesses the word, then the player loses.");
+
+        add(area);
 
         play = new JButton("Play");
         play.addActionListener(new ActionListener(){
@@ -30,6 +51,9 @@ public class InfoPanel extends JPanel {
                 cl.show(cards, "HANG");
             }
         });
-        add(play);
+
+        JPanel playP = new JPanel();
+        playP.add(play);
+        add(playP);
     }
 }
